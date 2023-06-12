42e30f6829c7d19975ab23a7dea57491e04945e8
Min Seo Han <65328995+nexmin0805@users.noreply.github.com>
2023-06-12 20:46:14 +0900

    자바스크립트 인라인기능

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -122,6 +122,20 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
     @Data
     static class User {
         private String username;
